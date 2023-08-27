b1ccf72916ef7cc96177b0f651780311dd354f2c
kjthorpe18 <kjt29@pitt.edu>
2023-08-27 18:11:07 -0400

    Format python files

--- stepcounts.py
+++ stepcounts.py
@@ -12,12 +12,12 @@ def plot_daily_counts(counts):
 
     fig = plt.gcf()
     fig.set_size_inches(30.5, 15.5)
-    fig.savefig('out/step_counts.png', dpi=100)
+    fig.savefig("out/step_counts.png", dpi=100)
     plt.clf()
 
 
 def main():
-    outdir = './out'
+    outdir = "./out"
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
@@ -25,35 +25,36 @@ def main():
 
     print("Extracting step counts...\n")
 
-    tree = ET.parse('data/export.xml')
+    tree = ET.parse("data/export.xml")
     root = tree.getroot()
 
     # Step counts are stored under an element like `<Record type="HKQuantityTypeIdentifierStepCount" .../>`
-    records_list = [x.attrib for x in root.iter('Record')]
+    records_list = [x.attrib for x in root.iter("Record")]
     df = pd.DataFrame(records_list)
-    step_counts = df[df['type'] == 'HKQuantityTypeIdentifierStepCount']
+    step_counts = df[df["type"] == "HKQuantityTypeIdentifierStepCount"]
 
     # Only use one device (e.g. Phone vs an Apple watch)
-    step_counts = step_counts[step_counts['sourceName'].str.contains("Phone")]
+    step_counts = step_counts[step_counts["sourceName"].str.contains("Phone")]
     step_counts = pd.DataFrame(step_counts)
 
     # Apple Health data is stored in XML tags as strings.
     # Rework strings to dates and numbers
-    for col in ['creationDate', 'startDate', 'endDate']:
+    for col in ["creationDate", "startDate", "endDate"]:
         step_counts[col] = pd.to_datetime(step_counts[col])
-        step_counts['date'] = step_counts[col].dt.date
+        step_counts["date"] = step_counts[col].dt.date
 
-    step_counts['value'] = pd.to_numeric(step_counts['value'])
+    step_counts["value"] = pd.to_numeric(step_counts["value"])
 
     print("Number of records: " + str(step_counts.shape[0]))
 
-    daily_counts = step_counts.groupby('date', as_index=False)['value'].sum()
+    daily_counts = step_counts.groupby("date", as_index=False)["value"].sum()
 
     # Output processed data as CSV
-    daily_counts.to_csv('out/dailycounts.csv')
-    step_counts.to_csv('out/stepcounts.csv')
+    daily_counts.to_csv("out/dailycounts.csv")
+    step_counts.to_csv("out/stepcounts.csv")
 
     plot_daily_counts(daily_counts)
+
 
 if __name__ == "__main__":
     main()
--- workouts.py
+++ workouts.py
@@ -23,14 +23,19 @@ def plot_workouts_pie(workouts):
         def my_autopct(pct):
             total = sum(values)
             val = int(round(pct * total / 100.0))
-            return '{p:.2f}%  ({v:d})'.format(p=pct, v=val)
+            return "{p:.2f}%  ({v:d})".format(p=pct, v=val)
 
         return my_autopct
 
     plt.figure(figsize=(10, 10))
-    plt.pie(slices, labels=labels, shadow=False,
-            startangle=90, autopct=make_autopct(slices),
-            wedgeprops={'edgecolor': 'black'})
+    plt.pie(
+        slices,
+        labels=labels,
+        shadow=False,
+        startangle=90,
+        autopct=make_autopct(slices),
+        wedgeprops={"edgecolor": "black"},
+    )
 
     plt.title("Workouts")
     plt.tight_layout()
@@ -38,7 +43,7 @@ def plot_workouts_pie(workouts):
 
 
 def main():
-    outdir = './dir'
+    outdir = "./dir"
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
@@ -47,32 +52,37 @@ def main():
     print("Extracting workout data...")
 
     # create element tree object
-    tree = ET.parse('data/export.xml')
+    tree = ET.parse("data/export.xml")
     root = tree.getroot()
 
     # Extract workouts from other Apple Health data and create a DataFrame
-    workout_list = [x.attrib for x in root.iter('Workout')]
+    workout_list = [x.attrib for x in root.iter("Workout")]
     workout_data = pd.DataFrame(workout_list)
 
     # Clean up the names of the columns and workouts. Example: HKWorkoutActivityTypeRunning -> Running
-    workout_data.drop('device', axis=1, inplace=True)
-    workout_data['workoutActivityType'].replace('HKWorkoutActivityType','', inplace=True, regex=True)
-    workout_data.rename(columns={'workoutActivityType': 'Type'}, inplace=True)
+    workout_data.drop("device", axis=1, inplace=True)
+    workout_data["workoutActivityType"].replace(
+        "HKWorkoutActivityType", "", inplace=True, regex=True
+    )
+    workout_data.rename(columns={"workoutActivityType": "Type"}, inplace=True)
 
     # Apple Health data is stored in XML tags as strings.
     # Rework strings to dates and numbers
-    for col in ['creationDate', 'startDate', 'endDate']:
+    for col in ["creationDate", "startDate", "endDate"]:
         workout_data[col] = pd.to_datetime(workout_data[col])
-        workout_data['date'] = workout_data[col].dt.date
+        workout_data["date"] = workout_data[col].dt.date
 
-    workout_data[['duration', 'totalEnergyBurned', 'totalDistance']].apply(pd.to_numeric, errors='coerce', axis=1)
+    workout_data[["duration", "totalEnergyBurned", "totalDistance"]].apply(
+        pd.to_numeric, errors="coerce", axis=1
+    )
 
     print("Number of records: " + str(workout_data.shape[0]))
 
     # Output processed data as CSV
-    workout_data.to_csv('out/workouts.csv')
-    durations = workout_data.groupby('date', as_index=False)['duration'].sum()
-    durations.to_csv('out/aggregated_workout_durations_per_day.csv')
+    workout_data.to_csv("out/workouts.csv")
+    durations = workout_data.groupby("date", as_index=False)["duration"].sum()
+    durations.to_csv("out/aggregated_workout_durations_per_day.csv")
+
 
 if __name__ == "__main__":
     main()
