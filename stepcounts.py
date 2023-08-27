bee4ff0a90c96b89d0256e552c82b14de55ce14c
kjthorpe18 <kjt29@pitt.edu>
2023-08-27 17:12:11 -0400

    Refactor main functions

--- stepcounts.py
+++ stepcounts.py
@@ -11,35 +11,38 @@ def plot_daily_counts(counts):
     plt.ylabel("Steps")
     plt.show()
 
+def main():
+    outdir = './out'
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
 
-outdir = './out'
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
+    plt.style.use("fivethirtyeight")
 
-plt.style.use("fivethirtyeight")
+    tree = ET.parse('data/export.xml')
+    root = tree.getroot()
 
-tree = ET.parse('data/export.xml')
-root = tree.getroot()
+    print("Extracting step counts...\n")
 
-print("Extracting step counts...\n")
+    records_list = [x.attrib for x in root.iter('Record')]
+    df = pd.DataFrame(records_list)
+    step_counts = df[df['type'] == 'HKQuantityTypeIdentifierStepCount']
+    step_counts = step_counts[step_counts['sourceName'].str.contains("Phone")]
+    step_counts = pd.DataFrame(step_counts)
 
-records_list = [x.attrib for x in root.iter('Record')]
-df = pd.DataFrame(records_list)
-step_counts = df[df['type'] == 'HKQuantityTypeIdentifierStepCount']
-step_counts = step_counts[step_counts['sourceName'].str.contains("Phone")]
-step_counts = pd.DataFrame(step_counts)
+    for col in ['creationDate', 'startDate', 'endDate']:
+        step_counts[col] = pd.to_datetime(step_counts[col])
+        step_counts['date'] = step_counts[col].dt.date
 
-for col in ['creationDate', 'startDate', 'endDate']:
-    step_counts[col] = pd.to_datetime(step_counts[col])
-    step_counts['date'] = step_counts[col].dt.date
+    step_counts['value'] = pd.to_numeric(step_counts['value'])
 
-step_counts['value'] = pd.to_numeric(step_counts['value'])
+    print("Number of records: " + str(step_counts.shape[0]))
 
-print("Number of records: " + str(step_counts.shape[0]))
+    daily_counts = step_counts.groupby('date', as_index=False)['value'].sum()
 
-daily_counts = step_counts.groupby('date', as_index=False)['value'].sum()
+    daily_counts.to_csv('out/dailycounts.csv')
+    step_counts.to_csv('out/stepcounts.csv')
 
-daily_counts.to_csv('out/dailycounts.csv')
-step_counts.to_csv('out/stepcounts.csv')
+    plot_daily_counts(daily_counts)
 
-plot_daily_counts(daily_counts)
+if __name__ == "__main__":
+    main()
--- workouts.py
+++ workouts.py
@@ -37,38 +37,42 @@ def plot_workouts(workouts):
     plt.show()
 
 
-outdir = './dir'
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
+def main():
+    outdir = './dir'
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
 
-plt.style.use("fivethirtyeight")
+    plt.style.use("fivethirtyeight")
 
-# create element tree object
-tree = ET.parse('data/export.xml')
+    # create element tree object
+    tree = ET.parse('data/export.xml')
 
-# for every health record, extract the attributes
-root = tree.getroot()
+    # for every health record, extract the attributes
+    root = tree.getroot()
 
-print("Extracting apple_health_analyzer...\n")
+    print("Extracting Workout data from export...\n")
 
-workout_list = [x.attrib for x in root.iter('Workout')]
+    workout_list = [x.attrib for x in root.iter('Workout')]
 
-workout_data = pd.DataFrame(workout_list)
-workout_data['workoutActivityType'] = workout_data['workoutActivityType'].str.replace('HKWorkoutActivityType', '')
-workout_data = workout_data.rename({"workoutActivityType": "Type"}, axis=1)
+    workout_data = pd.DataFrame(workout_list)
+    workout_data['workoutActivityType'] = workout_data['workoutActivityType'].str.replace('HKWorkoutActivityType', '')
+    workout_data = workout_data.rename({"workoutActivityType": "Type"}, axis=1)
 
-for col in ['creationDate', 'startDate', 'endDate']:
-    workout_data[col] = pd.to_datetime(workout_data[col])
-    workout_data['date'] = workout_data[col].dt.date
+    for col in ['creationDate', 'startDate', 'endDate']:
+        workout_data[col] = pd.to_datetime(workout_data[col])
+        workout_data['date'] = workout_data[col].dt.date
 
-workout_data['duration'] = pd.to_numeric(workout_data['duration'])
-workout_data['totalEnergyBurned'] = pd.to_numeric(workout_data['totalEnergyBurned'])
-workout_data['totalDistance'] = pd.to_numeric(workout_data['totalDistance'])
+    workout_data['duration'] = pd.to_numeric(workout_data['duration'])
+    workout_data['totalEnergyBurned'] = pd.to_numeric(workout_data['totalEnergyBurned'])
+    workout_data['totalDistance'] = pd.to_numeric(workout_data['totalDistance'])
 
-workout_data.drop('device', axis=1, inplace=True)
-workout_data.to_csv('out/apple_health_analyzer.csv')
+    workout_data.drop('device', axis=1, inplace=True)
+    workout_data.to_csv('out/apple_health_analyzer.csv')
 
-print("Number of apple_health_analyzer: " + str(workout_data.shape[0]))
+    print("Number of records: " + str(workout_data.shape[0]))
 
-durations = workout_data.groupby('date', as_index=False)['duration'].sum()
-durations.to_csv('out/aggregated_durations.csv')
+    durations = workout_data.groupby('date', as_index=False)['duration'].sum()
+    durations.to_csv('out/aggregated_durations.csv')
+
+if __name__ == "__main__":
+    main()
